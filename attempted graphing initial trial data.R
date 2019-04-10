a3031e87535516f98558749b4186f6c7a652ecfc
Andrew Baxter <andrewbaxter439@gmail.com>
2019-04-10 17:58:31 +0100

    Working two-mode plot

--- attempted graphing initial trial data.R
+++ attempted graphing initial trial data.R
@@ -1,6 +1,5 @@
 library(tidyverse)
 library(readxl)
-library(magrittr)
 library(igraph)
 
 
@@ -14,6 +13,7 @@ colnames(trial1) <- colnames(trial1) %>% gsub(" ", "_", .)  # change spaces to '
 
 trial2 <- trial1 %>%   # remove rows heading up each document, remove duplicates, fill in 0s for now
   filter(!is.na(WHO_2014)) %>% 
+  select(-NHS_2017, -ACS_2018) %>% 
   unique() %>% 
   replace(is.na(.), 0)
 
@@ -26,14 +26,21 @@ trial_matrix <- trial2 %>%  # rest of columns into matrix
 row.names(trial_matrix) <- row_names  # add rownames back in
 
 imatrix <- graph.incidence(trial_matrix, mode = "all")  # igraph matrix
-#imatrix <- delete.vertices(imatrix, V(imatrix)[degree(imatrix)==0])
-V(imatrix)$color[1:1293] <- "#FF0000FF"
-V(imatrix)$color[1294:1311] <- "#00FF0080"
+imatrix <- delete.vertices(imatrix, V(imatrix)[degree(imatrix)==0])
+V(imatrix)$color[1:1288] <- "#FF0000FF"
+V(imatrix)$color[1289:1305] <- "#00FF0080"
 
-V(imatrix)$label[1:1291] <- NA
-
+V(imatrix)$label[1:1288] <- NA
+V(imatrix)$label[1289:1305] <- V(imatrix)$name[1289:1305]
+V(imatrix)$size[1:1288] <- 2
+V(imatrix)$size[1289:1305] <- 10
+V(imatrix)$frame.color <- NA
+V(imatrix)$label.cex <- 0.4
+V(imatrix)
 V(imatrix)
 
-#pdf('imatrix.pdf')
-plot(imatrix, layout = layout.fruchterman.reingold)
-#dev.off()
+
+
+# pdf('imatrix3.pdf')
+plot(imatrix, layout = layout.kamada.kawai)
+#
