d7d7b4d5e75ab31ce5df26e32ce8ed18141130df
Andrew Baxter <andrewbaxter439@gmail.com>
2019-10-29 11:44:43 +0000

    Nrefs graph added to slides, new updated PECO.docx

--- R/7 - graph of guidelines sized by number of references.R
+++ R/7 - graph of guidelines sized by number of references.R
@@ -20,7 +20,7 @@ ccols <- tibble(context =  as.factor(c("WHO",
 # guidline sizes ---------------------------------------------------------------------------------------------
 
 
-dref_df <- dfe %>% 
+dref_df <-  dfe %>% 
   select(2:17) %>% 
   summarise_all(~ sum(.x)) %>% 
   gather("Guideline", "Nrefs") %>% 
@@ -31,17 +31,17 @@ dref_df <- dfe %>%
                                          rep("UK", 7),
                                          rep("AUS", 4),
                                          rep("USA", 3),
-                                         "UK", "AUS")))) %>% 
+                                         "UK", "USA")))) %>% 
   full_join(ccols, by = "context") %>% 
   # mutate(context = factor(context, ordered = TRUE)) %>% 
   group_by(context) %>% 
   mutate(n_gl = n()) %>% 
-  ungroup() %>% 
+  ungroup() %>%
   arrange(desc(n_gl), context, desc(Nrefs)) %>% 
   group_by(context) %>% 
   mutate(rank = 1:n_gl) %>% 
   ungroup() %>% 
-  mutate(n_con = c(rep(1,8), rep(2, 5), rep(3, 3), rep(4, 2)),
+  mutate(n_con = c(rep(1,8), rep(2, 4), rep(3, 4), rep(4, 2)),
          label = paste0(Guideline, "\n(", Nrefs, ")"))
 
 mt <- diag(nrow = nrow(dref_df), ncol = nrow(dref_df))
@@ -53,15 +53,16 @@ colnames(mt) <- dref_df$Guideline
 
 ig <- graph.adjacency(mt) 
 
-V(ig)$size <- log10(dref_df$Nrefs+1)*10
+# V(ig)$size <- (dref_df$Nrefs+1)/50
+V(ig)$size <- log10(dref_df$Nrefs+1)*8
 V(ig)$label <- dref_df$label
 V(ig)$frame.color <- NA
 V(ig)$color <- dref_df$fill
-V(ig)$label.cex <- 0.5
+V(ig)$label.cex <- 0.7
 
 E(ig)$color <- NA
 
-layout <-  as.matrix(tibble(a = (dref_df$rank-1)*16/50, b = (27 - (dref_df$n_con-1)*9)/30))
+layout <-  as.matrix(tibble(a = (dref_df$rank-1)/4, b = (3 - (dref_df$n_con-1))/4))
 svg("./graphs/bubbles sized by nrefs.svg")
 plot(ig,
      layout = layout, 
